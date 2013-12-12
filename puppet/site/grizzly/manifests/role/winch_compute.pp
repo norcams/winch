cd52bb14e9b98538fb58c3817b12ba68bdc48b17
Jan Ivar Beddari <code@beddari.net>
2013-12-12 14:19:53 +0100

    Moved router to controller

--- puppet/site/grizzly/manifests/role/winch_compute.pp
+++ puppet/site/grizzly/manifests/role/winch_compute.pp
@@ -1,5 +1,4 @@
 class grizzly::role::winch_compute inherits ::grizzly::role {
-  class { '::grizzly::profile::quantum::router': } ->
   class { '::grizzly::profile::quantum::agent': } ->
   class { '::grizzly::profile::nova::compute': }
 }
--- puppet/site/grizzly/manifests/role/winch_controller.pp
+++ puppet/site/grizzly/manifests/role/winch_controller.pp
@@ -5,5 +5,6 @@ class grizzly::role::winch_controller inherits ::grizzly::role {
   class { '::grizzly::profile::keystone': } ->
   class { '::grizzly::profile::glance::auth': } ->
   class { '::grizzly::profile::glance::api': } ->
+  class { '::grizzly::profile::quantum::router': } ->
   class { '::grizzly::profile::nova::api': }
 }
