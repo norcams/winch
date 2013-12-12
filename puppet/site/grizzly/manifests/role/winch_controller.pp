3eeffa73137eee9016c0afc58a6707f296c54a22
Jan Ivar Beddari <code@beddari.net>
2013-12-12 21:29:14 +0100

    Added missing neutron server profile to winch_controller

--- puppet/site/grizzly/manifests/role/winch_controller.pp
+++ puppet/site/grizzly/manifests/role/winch_controller.pp
@@ -5,6 +5,7 @@ class grizzly::role::winch_controller inherits ::grizzly::role {
   class { '::grizzly::profile::keystone': } ->
   class { '::grizzly::profile::glance::auth': } ->
   class { '::grizzly::profile::glance::api': } ->
+  class { '::grizzly::profile::quantum::server': } ->
   class { '::grizzly::profile::quantum::router': } ->
   class { '::grizzly::profile::nova::api': }
 }
