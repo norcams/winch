096500221a359f3d939473ce5ab51b240edbe105
Jan Ivar Beddari <code@beddari.net>
2014-01-14 16:15:43 +0100

    Added config and instructions for building documentation locally

---
+++ docs/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Run these commands (or equivalent) to set up sphinx locally:"
+echo ""
+echo "pip install sphinx"
+echo "pip install sphinx_rtd_theme"
+echo ""
+echo "Then run 'make html' to build the documentation"
+
