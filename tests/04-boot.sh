64eda663a015dc079bc6638bae1855846a9e1c92
Jan Ivar Beddari <code@beddari.net>
2014-01-30 09:32:19 +0100

    Only match the first image ID when testing boot

--- tests/04-boot.sh
+++ tests/04-boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -vx
-CIRROS_IMAGE_ID=$(nova image-list | awk '/CirrOS/ { print $2 }')
+CIRROS_IMAGE_ID=$(nova image-list | awk '/CirrOS/ { print $2 }' | head -n 1)
 nova boot --flavor 1 --image ${CIRROS_IMAGE_ID} --security_group test test-1
