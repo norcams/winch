225d0f7446fe7a9af1010711f9cecdb51c9b9527
Jan Ivar Beddari <code@beddari.net>
2014-01-30 12:05:34 +0100

    Updated security group testing to quantum/neutron syntax

--- tests/03-create_security_group_and_rules.sh
+++ tests/03-create_security_group_and_rules.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -vx
-nova secgroup-create test "test security group"
-nova secgroup-add-rule test tcp 22 22 0.0.0.0/0
-nova secgroup-add-rule test icmp -1 -1 0.0.0.0/0
+# Old nova commands:
+#nova secgroup-create test "test security group"
+#nova secgroup-add-rule test tcp 22 22 0.0.0.0/0
+#nova secgroup-add-rule test icmp -1 -1 0.0.0.0/0
+quantum security-group-create test
+quantum security-group-rule-create --direction ingress --protocol tcp --port_range_min 22 --port_range_max 22 test
+quantum security-group-rule-create --protocol icmp --direction ingress test
