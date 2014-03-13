69a9fea53a5707ef57e2c331bab37ed5796415d4
Jan Ivar Beddari <code@beddari.net>
2014-03-13 08:39:38 +0100

    Removed grizzly modules tree from puppet/modules

--- puppet/modules/nova/manifests/utilities.pp
+++
@@ -1,6 +0,0 @@
-# unzip swig screen parted curl euca2ools - extra packages
-class nova::utilities {
-  if $::osfamily == 'Debian' {
-    ensure_packages(['unzip', 'screen', 'parted', 'curl', 'euca2ools'])
-  }
-}
