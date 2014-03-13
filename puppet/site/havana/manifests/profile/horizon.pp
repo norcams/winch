18912404c09631a031d4d4b7a51e71242c1e253d
Jan Ivar Beddari <code@beddari.net>
2014-03-13 13:08:59 +0100

    Fix/workaround that allows all hosts for Horizon

--- puppet/site/havana/manifests/profile/horizon.pp
+++ puppet/site/havana/manifests/profile/horizon.pp
@@ -1,7 +1,7 @@
 # Profile to install the horizon web service
 class havana::profile::horizon {
   class { '::horizon':
-    fqdn            => [ '127.0.0.1', hiera('havana::controller::address::api'), $::fqdn ],
+    fqdn            => [ '127.0.0.1', hiera('havana::controller::address::api'), $::fqdn, '*' ],
     secret_key      => hiera('havana::horizon::secret_key'),
     cache_server_ip => hiera('havana::controller::address::management'),
 
