14d938cf74d26727f0fa4c9e600bf38ca0f90637
Jan Ivar Beddari <code@beddari.net>
2014-03-13 13:44:49 +0100

    Specify testnet with --nic option when booting in test script

--- tests/04-boot.sh
+++ tests/04-boot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -vx
-CIRROS_IMAGE_ID=$(nova image-list | awk '/CirrOS/ { print $2 }' | head -n 1)
-nova boot --flavor 1 --image ${CIRROS_IMAGE_ID} --security_group test test-1
+IMAGE_ID=$(nova image-list | awk '/CirrOS/ { print $2 }' | head -n 1)
+NET_ID=$(neutron net-show testnet | grep " id " | awk '{ print $4 }')
+nova boot --flavor 1 --image ${IMAGE_ID} --nic net-id=${NET_ID} --security_group test test-1
