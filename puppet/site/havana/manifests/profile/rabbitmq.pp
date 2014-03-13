7194ab97e24aae1cadbca9eaec6f342272e92c0a
Jan Ivar Beddari <code@beddari.net>
2014-03-13 11:13:16 +0100

    Work around/hack to get an array param for rabbitmq module

--- puppet/site/havana/manifests/profile/rabbitmq.pp
+++ puppet/site/havana/manifests/profile/rabbitmq.pp
@@ -3,6 +3,7 @@ class havana::profile::rabbitmq {
   class { '::nova::rabbitmq':
     userid             => hiera('havana::rabbitmq::user'),
     password           => hiera('havana::rabbitmq::password'),
-    cluster_disk_nodes => hiera('havana::controller::address::management'),
+    #cluster_disk_nodes => hiera('havana::controller::address::management'),
+    cluster_disk_nodes => [ '172.16.188.11' ],
   }
 }
