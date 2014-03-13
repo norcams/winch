66eff1e2fde119c5bd8c3760d20bdaa952630c5b
Jan Ivar Beddari <code@beddari.net>
2014-03-13 09:38:48 +0100

    Added winch roles for Havana

---
+++ puppet/site/havana/manifests/role/winch_compute.pp
@@ -0,0 +1,6 @@
+class havana::role::winch_compute inherits ::havana::role {
+  class { '::havana::profile::firewall': }
+  class { '::havana::profile::neutron::agent': }
+  class { '::havana::profile::nova::compute': }
+  class { '::havana::profile::ceilometer::agent': }
+}
