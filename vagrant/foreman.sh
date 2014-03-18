b959599ff589d05c4eb57f9ebd424880d31fe3b5
Jan Ivar Beddari <code@beddari.net>
2014-03-18 14:09:07 +0100

    Added Foreman test-build
    * Scripts to install Foreman with foreman-installer
    * Updated Vagrantfile with an 'installer' node

---
+++ vagrant/foreman.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /vagrant/vagrant/foreman_add_repo.sh
+source /vagrant/vagrant/foreman_install.sh
+source /vagrant/vagrant/foreman_configure.sh
+source /vagrant/vagrant/foreman_netfwd.sh
+echo
+echo "Manual settings that needs to be changed:"
+echo
+echo "Administer->Settings->Provisioning:"
+echo "query_local_nameservers = true"
+echo
+echo "Hosts->Provisioning Templates->Kickstart default"
+echo "Add 'unsupported_hardware' as a property after 'install'"
+echo
---
+++ vagrant/foreman_configure.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+hammer proxy create --name "controller" \
+  --url "https://controller.vagrant.local:8443"
+hammer proxy info --name "controller"
+
+# Create a domain named vagrant.local and associate it with DNS on proxy id 1
+hammer domain create --name "vagrant.local" --dns-id 1
+hammer domain info --name "vagrant.local"
+
+hammer subnet create --name "vagrant" \
+  --network "172.16.88.0" \
+  --mask "255.255.255.0" \
+  --gateway "172.16.88.11" \
+  --dns-primary "172.16.88.11"
+hammer subnet update --name "vagrant" \
+  --domain-ids 1 --dhcp-id 1 --tftp-id 1 --dns-id 1
+hammer subnet info --name "vagrant"
+
+hammer environment create --name "production"
+hammer environment info --name "production"
+
+hammer os create --name CentOS --major 6 --minor 5 --description "CentOS 6.5" --family Redhat --architecture-ids 1 --medium-ids 1 --ptable-ids 6
+hammer template update --id 13 --operatingsystem-ids 1
+hammer template update --id 11 --operatingsystem-ids 1
+
+# work around Puppet bug #2244 which is fixed in 3.x
+sudo mkdir -p /etc/puppet/environments/common/dummy/lib
+
