fd451dd92b1cf84f2ea4b441a26658407fd76ce6
Jan Ivar Beddari <code@beddari.net>
2014-03-25 09:42:23 +0100

    Basic cinder demo/test script

---
+++ tests/10-cinder.sh
@@ -0,0 +1,28 @@
+echo CINDER TEST - copy and paste :-)
+echo
+echo sudo vgdisplay
+echo sudo losetup -a
+echo sudo pvdisplay
+echo sudo lvdisplay
+echo
+echo "List only cinder-volumes:"
+echo sudo lvdisplay cinder-volumes
+echo
+echo "Create a cinder volume:"
+echo cinder create --display-name testvolume 1
+echo "- Look, its magic:"
+echo sudo lvdisplay cinder-volumes
+echo 
+echo "Attaching a volume to an instance:"
+echo "nova volume-attach <instance-id> <volume-id> /dev/vdc"
+echo "- Log in and verify ..."
+echo
+echo "Detaching:"
+echo "nova volume-list"
+echo "- Make sure the volume is unmounted"
+echo "- Then detach with"
+echo "nova volume-detach <instance-id> <volume_id>"
+echo
+echo "Deleting a volume:"
+echo "nova volume-delete <volume_id>"
+
