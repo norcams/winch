eee693120dfc69204ed11eaa02779cb5e45b8985
edpto <tor.ladre@it.uib.no>
2014-09-09 15:58:21 +0200

    Modified tests to accomodate changes in adresses etc

--- tests/00-testuser.sh
+++ tests/00-testuser.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+export OS_USERNAME=demo
 export OS_TENANT_NAME=test
-export OS_USERNAME=test
 export OS_PASSWORD=abc123
-export OS_AUTH_URL=http://192.168.166.11:5000/v2.0
+export OS_AUTH_URL=http://192.168.11.12:5000/v2.0/
+export PS1='[\u@\h \W(keystone_demo)]\$ '
 export OS_NO_CACHE=1
 
 # Run keystone to get tenant-id - tenant is already created by Puppet
-TENANT_ID=$(keystone tenant-list | awk '/\ test\ / { print $2 }')
-
+TENANT_ID=$(keystone tenant-list | awk '/\ demo\ / { print $2 }')
--- tests/05-create_provider_network_and_subnet.sh
+++ tests/05-create_provider_network_and_subnet.sh
@@ -2,8 +2,8 @@
 neutron net-create --router:external=True floatingnet
 neutron subnet-create \
   --name floatingsubnet \
-  --allocation-pool start=192.168.177.20,end=192.168.177.29 \
-  --gateway 192.168.177.11 \
+  --allocation-pool start=192.168.22.20,end=192.168.22.29 \
+  --gateway 192.168.22.11 \
   --enable_dhcp=False \
-  floatingnet 192.168.177.0/24
+  floatingnet 192.168.22.0/24
 
