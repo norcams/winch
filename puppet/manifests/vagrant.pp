7ee4d51ebb988ed448dcf081dc2721bbf94b6974
edpto <tor.ladre@it.uib.no>
2014-09-09 15:16:16 +0200

    Updated for post-Havana OpenStack

--- puppet/manifests/vagrant.pp
+++ puppet/manifests/vagrant.pp
@@ -1,8 +1,8 @@
 node controller {
-  include ::havana::role::winch_controller
+  include ::openstack::role::winch_controller
 }
 
 node compute {
-  include ::havana::role::winch_compute
+  include ::openstack::role::winch_compute
 }
 
