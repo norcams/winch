e87f555317d1c275cb173b698512c88265ccb81d
edpto <tor.ladre@it.uib.no>
2014-10-03 09:54:58 +0200

    Changed if for in-traffic

--- vagrant/foreman_netfwd.sh
+++ vagrant/foreman_netfwd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -vx
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
-sudo iptables --append FORWARD --in-interface eth3 -j ACCEPT
+sudo iptables --append FORWARD --in-interface eth1 -j ACCEPT
