4b6bfbcbe7f97042973239d787ebca3efa0dc0d0
Kristian Aadlandsvik Skurtveit <ksk037@it011100.klientdrift.uib.no>
2014-10-06 08:47:03 +0200

    Fixed VirtualBox storage location for harddrives

--- vagrant/create-vbox-compute.sh
+++ vagrant/create-vbox-compute.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+TPATH=`VBoxManage list systemproperties | grep -i "default machine folder:" \
+    | cut -b 24- | awk '{gsub(/^ +| +$/,"")}1'`
+VMNAME="compute"
+VMPATH="$TPATH/$VMNAME"
 
-## Compute
-VBoxManage createvm --name "compute" --register --ostype RedHat_64
-VBoxManage modifyvm "compute" --memory 2048 --acpi on --cpuexecutioncap 100 --cpus 2 --boot1 disk --boot2 dvd
+VBoxManage createvm --name "$VMNAME" --register --ostype RedHat_64
+VBoxManage modifyvm "$VMNAME" --memory 2048 --acpi on --cpuexecutioncap 100 --cpus 2 --boot1 disk --boot2 dvd
 
-VBoxManage modifyvm "compute" --nic1 hostonly --hostonlyadapter1 vboxnet0 --cableconnected1 on
-VBoxManage modifyvm "compute" --macaddress1 auto --nictype2 82540EM
+VBoxManage modifyvm "$VMNAME" --nic1 hostonly --hostonlyadapter1 vboxnet0 --cableconnected1 on
+VBoxManage modifyvm "$VMNAME" --macaddress1 auto --nictype2 82540EM
 
-VBoxManage modifyvm "compute" --nic2 hostonly --hostonlyadapter2 vboxnet1 --cableconnected2 on
-VBoxManage modifyvm "compute" --macaddress2 auto --nictype1 82540EM
+VBoxManage modifyvm "$VMNAME" --nic2 hostonly --hostonlyadapter2 vboxnet1 --cableconnected2 on
+VBoxManage modifyvm "$VMNAME" --macaddress2 auto --nictype1 82540EM
 
-VBoxManage createhd --filename ./compute.vdi --size 8192
-VBoxManage storagectl "compute" --name "IDE Controller" --add ide
-
-VBoxManage storageattach "compute" --storagectl "IDE Controller"  \
-    --port 0 --device 0 --type hdd --medium ./compute.vdi
+VBoxManage createhd --filename "$VMPATH/$VMNAME.vdi" --size 8196
+VBoxManage storagectl "$VMNAME" --name "SATA Controller" \
+    --add sata --controller IntelAHCI --hostiocache on --bootable on
+VBoxManage storageattach "$VMNAME" --storagectl "SATA Controller" \
+    --type hdd --port 0 --device 0 --medium "$VMPATH/$VMNAME.vdi"
--- vagrant/create-vbox-controller.sh
+++ vagrant/create-vbox-controller.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+TPATH=`VBoxManage list systemproperties | grep -i "default machine folder:" \
+    | cut -b 24- | awk '{gsub(/^ +| +$/,"")}1'`
+VMNAME="controller"
+VMPATH="$TPATH/$VMNAME"
 
 ## Controller
-VBoxManage createvm --name "controller" --register --ostype RedHat_64
-VBoxManage modifyvm "controller" --memory 2048 --acpi on --cpus 2 --cpuexecutioncap 100 --boot1 disk --boot2 dvd
+VBoxManage createvm --name "$VMNAME" --register --ostype RedHat_64
+VBoxManage modifyvm "$VMNAME" --memory 2048 --acpi on --cpus 2 --cpuexecutioncap 100 --boot1 disk --boot2 dvd
 
-VBoxManage modifyvm "controller" --nic1 hostonly --hostonlyadapter1 vboxnet0 --cableconnected1 on
-VBoxManage modifyvm "controller" --macaddress1 auto --nictype2 82540EM
+VBoxManage modifyvm "$VMNAME" --nic1 hostonly --hostonlyadapter1 vboxnet0 --cableconnected1 on
+VBoxManage modifyvm "$VMNAME" --macaddress1 auto --nictype2 82540EM
 
-VBoxManage modifyvm "controller" --nic2 hostonly --hostonlyadapter2 vboxnet1 --cableconnected2 on
-VBoxManage modifyvm "controller" --macaddress2 auto --nictype1 82540EM
+VBoxManage modifyvm "$VMNAME" --nic2 hostonly --hostonlyadapter2 vboxnet1 --cableconnected2 on
+VBoxManage modifyvm "$VMNAME" --macaddress2 auto --nictype1 82540EM
 
-VBoxManage createhd --filename ./controller.vdi --size 40960
-VBoxManage storagectl "controller" --name "IDE Controller" --add ide
-
-VBoxManage storageattach "controller" --storagectl "IDE Controller"  \
-    --port 0 --device 0 --type hdd --medium ./controller.vdi
+VBoxManage createhd --filename "$VMPATH/$VMNAME.vdi" --size 40960
+VBoxManage storagectl "$VMNAME" --name "SATA Controller" \
+    --add sata --controller IntelAHCI --hostiocache on --bootable on
+VBoxManage storageattach "$VMNAME" --storagectl "SATA Controller" \
+    --type hdd --port 0 --device 0 --medium "$VMPATH/$VMNAME.vdi"
 
