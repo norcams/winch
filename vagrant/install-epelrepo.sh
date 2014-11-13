fdd6a5ec85153bcf48c59a5abb2cec9c05e7a6be
edpto <tor.ladre@it.uib.no>
2014-11-13 18:54:30 +0100

    Always install epel repo

---
+++ vagrant/install-epelrepo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rhelversion=$(cat /etc/redhat-release | grep release\ 7)
+
+if [ -n "$rhelversion" ]; then
+    /bin/yum -y install http://ftp.uninett.no/linux/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+else
+    /bin/yum -y install http://ftp.uninett.no/linux/epel/6/i386/epel-release-6-8.noarch.rpm
+fi
