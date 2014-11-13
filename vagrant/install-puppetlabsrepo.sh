351b6e33417333c00bb5736166e611095c993aff
edpto <tor.ladre@it.uib.no>
2014-11-13 16:09:01 +0100

    Fixed installation of puppet and facter on CentOS7, added comments in script

--- vagrant/install-puppetlabsrepo.sh
+++ vagrant/install-puppetlabsrepo.sh
@@ -2,9 +2,13 @@
 
 rhelversion=$(cat /etc/redhat-release | grep release\ 7)
 
+# For CentOS7/RHEL7 the rdo release contains puppetlabs repo, creating conflict
 if [ -n "$rhelversion" ]; then
     /bin/rpm -ivh https://repos.fedorapeople.org/repos/openstack/openstack-icehouse/epel-7/rdo-release-icehouse-4.noarch.rpm
 else
     /bin/rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-    /usr/bin/yum install -y puppet facter
 fi
+
+# Install puppet and facter regardless
+    /usr/bin/yum install -y puppet facter
+
