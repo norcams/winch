005fa947a7889b11740d924c4376cb8d5285b241
edpto <tor.ladre@it.uib.no>
2014-11-13 16:00:00 +0100

    Facilitated pre-installation of rdo repo for centos 7 to avoid conflict with puppetlabs repo package

--- vagrant/install-puppetlabsrepo.sh
+++ vagrant/install-puppetlabsrepo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# 
-/bin/rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-/usr/bin/yum install -y puppet facter
+rhelversion=$(cat /etc/redhat-release | grep release\ 7)
 
+if [ -n "$rhelversion" ]; then
+    /bin/rpm -ivh https://repos.fedorapeople.org/repos/openstack/openstack-icehouse/epel-7/rdo-release-icehouse-4.noarch.rpm
+else
+    /bin/rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+    /usr/bin/yum install -y puppet facter
+fi
