50480327d7f823d61a561a901248c61fdba0279a
ksk037 <kristian.skurtveit@gmail.com>
2014-11-24 12:54:40 +0100

    Create keystonerc files for all testusers. Admin, demo and demo2

--- tests/00-testuser.sh
+++ tests/00-testuser.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-export OS_USERNAME=test
+
+# Create keystonerc_admin and keystonerc_demo keystonerc_demo2 files
+echo "export OS_USERNAME=test
+export OS_TENANT_NAME=test
+export OS_PASSWORD=abc123
+export OS_AUTH_URL=http://192.168.11.12:5000/v2.0/
+export PS1='[\u@\h \W(keystone_admin)]\$ '
+export OS_NO_CACHE=1" > /root/keystonerc_admin
+
+echo "export OS_USERNAME=demo
 export OS_TENANT_NAME=test
 export OS_PASSWORD=abc123
 export OS_AUTH_URL=http://192.168.11.12:5000/v2.0/
 export PS1='[\u@\h \W(keystone_demo)]\$ '
-export OS_NO_CACHE=1
+export OS_NO_CACHE=1" > /home/vagrant/keystonerc_demo
 
-# Run keystone to get tenant-id - tenant is already created by Puppet
-TENANT_ID=$(keystone tenant-list | awk '/\ demo\ / { print $2 }')
+echo "export OS_USERNAME=demo2
+export OS_TENANT_NAME=test
+export OS_PASSWORD=abc123
+export OS_AUTH_URL=http://192.168.11.12:5000/v2.0/
+export PS1='[\u@\h \W(keystone_demo2)]\$ '
+export OS_NO_CACHE=1" > /home/vagrant/keystonerc_demo2
+
+echo "Keystonerc file locations:"
+echo "/root/keystonerc_admin"
+echo "/home/vagrant/keystonerc_demo"
+echo "/home/vagrant/keystonerc_demo2"
+
+
