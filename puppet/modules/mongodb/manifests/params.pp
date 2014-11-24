d5a27f85764772ae8f4ebc014a054f2ad932ada1
edpto <tor.ladre@it.uib.no>
2014-11-24 15:23:32 +0100

    Fixed bug i mongodb module triggered on centos 7

--- puppet/modules/mongodb/manifests/params.pp
+++ puppet/modules/mongodb/manifests/params.pp
@@ -57,7 +57,7 @@ class mongodb::params inherits mongodb::globals {
         $dbpath              = '/var/lib/mongodb'
         $logpath             = '/var/log/mongodb/mongodb.log'
         $bind_ip             = pick($::mongodb::globals::bind_ip, ['127.0.0.1'])
-        $pidfilepath         = '/var/run/mongodb/mongodb.pid'
+        $pidfilepath         = '/var/run/mongodb/mongod.pid'
         $fork                = true
         $journal             = true
       }
