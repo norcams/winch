a5e425b0a81b982e382c7348389899a11b01b276
edpto <tor.ladre@it.uib.no>
2014-11-25 13:02:29 +0100

    Updated puppet install for manager to support el7

--- vagrant/foreman_puppet.sh
+++ vagrant/foreman_puppet.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo yum -y install http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-11.noarch.rpm
-sudo yum -y install puppet facter
+rhelversion=$(cat /etc/redhat-release | grep release\ 7)
 
+if [ -n "$rhelversion" ]; then
+    /bin/rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+    /bin/yum install -y puppet facter
+else
+    /bin/rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+    /usr/bin/yum install -y puppet facter
+fi
