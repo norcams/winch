df20cfaebd0b5cfef039bf6db1a14d1533ab3f33
edpto <tor.ladre@it.uib.no>
2014-11-25 13:51:42 +0100

    Removed redundant addition of rdo repository

--- vagrant/install-puppetlabsrepo.sh
+++ vagrant/install-puppetlabsrepo.sh
@@ -30,7 +30,7 @@ rm -rf /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs
 rm -rf /etc/yum.repos.d/puppettemp.repo
 
 # install openstack rdo-repo
-rpm -i https://repos.fedorapeople.org/repos/openstack/openstack-icehouse/epel-7/rdo-release-icehouse-4.noarch.rpm
+#rpm -i https://repos.fedorapeople.org/repos/openstack/openstack-icehouse/epel-7/rdo-release-icehouse-4.noarch.rpm
 
 else
     /bin/rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
