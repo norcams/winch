e5206cf0a93e4af1c96491b7589f9e113638165d
edpto <tor.ladre@it.uib.no>
2014-11-25 17:24:13 +0100

    Fixed path typo for el6

--- vagrant/install-epelrepo.sh
+++ vagrant/install-epelrepo.sh
@@ -5,5 +5,5 @@ rhelversion=$(cat /etc/redhat-release | grep release\ 7)
 if [ -n "$rhelversion" ]; then
     /bin/yum -y install http://ftp.uninett.no/linux/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
 else
-    /bin/yum -y install http://ftp.uninett.no/linux/epel/6/i386/epel-release-6-8.noarch.rpm
+    /usr/bin/yum -y install http://ftp.uninett.no/linux/epel/6/i386/epel-release-6-8.noarch.rpm
 fi
