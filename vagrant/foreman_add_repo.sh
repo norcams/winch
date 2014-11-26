fb76e5dba8b07429b3d7acef236bbe9bfec1c7d9
edpto <tor.ladre@it.uib.no>
2014-11-26 07:20:54 +0100

    Rebased foreman installer to el6

--- vagrant/foreman_add_repo.sh
+++ vagrant/foreman_add_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo yum -y install http://yum.theforeman.org/releases/1.7/el7/x86_64/foreman-release.rpm
+sudo yum -y install http://yum.theforeman.org/releases/1.7/el6/x86_64/foreman-release.rpm
 sudo yum -y install foreman-installer
