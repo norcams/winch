300873aa0f474aee075cfaa4567396c835028754
edpto <tor.ladre@it.uib.no>
2014-12-17 16:17:52 +0100

    Updated for newer epel7 release

--- vagrant/install-epelrepo.sh
+++ vagrant/install-epelrepo.sh
@@ -3,7 +3,7 @@
 rhelversion=$(cat /etc/redhat-release | grep release\ 7)
 
 if [ -n "$rhelversion" ]; then
-    /bin/yum -y install http://ftp.uninett.no/linux/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+    /bin/yum -y install http://ftp.uninett.no/linux/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 else
     /usr/bin/yum -y install http://ftp.uninett.no/linux/epel/6/i386/epel-release-6-8.noarch.rpm
 fi
