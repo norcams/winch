b4da0fb4cfe4154bec307671ca9c5698a3a113c6
ksk037 <kristian.skurtveit@gmail.com>
2015-03-07 18:24:21 +0100

    Added submodule archive

---
+++ puppet/modules/archive/CHANGELOG.md
@@ -0,0 +1,46 @@
+## 2015-03-06 - Release 0.4.0
+
+- Add --strip-components support
+
+## 2015-01-07 - Release 0.3.6
+
+- Fix unquoted strings in cases
+
+## 2015-01-05 - Release 0.3.5
+
+- Simplify bundler cache in Travis CI
+- Fix license name in metadata.json
+
+##2014-11-18 Release 0.3.2
+- Linting metadata
+
+##2014-11-04 Release 0.3.1
+- Fix missing comma
+
+##2014-11-04 Release 0.3.0
+- Add path to execs
+
+##2014-11-04 Release 0.2.1
+- Drop Puppet 2.7 support
+
+##2014-10-20 Release 0.2.0
+### Summary
+- Setup automatic Forge releases
+
+##2014-09-24 Release 0.1.3
+###Summary
+- Bug fix
+
+##2014-09-23 Release 0.1.2
+###Summary
+- Quote url to get the data from
+
+##2014-09-05 Release 0.1.1
+###Summary
+- Allow disabling of no-checksum notices in puppetmaster logs
+
+##2014-07-02 Release 0.1.0
+###Summary
+- Make curl silent, #21
+- Add documentation
+- Fix strict variables
---
+++ puppet/modules/archive/manifests/init.pp
@@ -0,0 +1,72 @@
+# == Definition: archive
+#
+# Download and extract an archive.
+#
+# Parameters:
+#
+# - *$url:
+# - *$target: Destination directory
+# - *$checksum: Default value "true"
+# - *$digest_url: Default value undef
+# - *$digest_string: Default value undef
+# - *$digest_type: Default value "md5"
+# - *$src_target: Default value "/usr/src"
+# - *$root_dir: Default value undef
+# - *$extension: Default value ".tar.gz"
+# - *$timeout: Default value 120
+# - *$allow_insecure: Default value false
+# - *$follow_redirects: Default value false
+# - *$verbose: Default value true
+# - *$strip_components: Default value 0
+#
+# Example usage:
+#
+#   archive {"apache-tomcat-6.0.26":
+#     ensure => present,
+#     url    => "http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.tar.gz",
+#     target => "/opt",
+#   }
+#
+define archive (
+  $url,
+  $target,
+  $ensure=present,
+  $checksum=true,
+  $digest_url=undef,
+  $digest_string=undef,
+  $digest_type='md5',
+  $timeout=120,
+  $root_dir=undef,
+  $extension='tar.gz',
+  $src_target='/usr/src',
+  $allow_insecure=false,
+  $follow_redirects=false,
+  $verbose=true,
+  $strip_components=0,
+) {
+
+  archive::download {"${name}.${extension}":
+    ensure           => $ensure,
+    url              => $url,
+    checksum         => $checksum,
+    digest_url       => $digest_url,
+    digest_string    => $digest_string,
+    digest_type      => $digest_type,
+    timeout          => $timeout,
+    src_target       => $src_target,
+    allow_insecure   => $allow_insecure,
+    follow_redirects => $follow_redirects,
+    verbose          => $verbose,
+  }
+
+  archive::extract {$name:
+    ensure           => $ensure,
+    target           => $target,
+    src_target       => $src_target,
+    root_dir         => $root_dir,
+    extension        => $extension,
+    timeout          => $timeout,
+    strip_components => $strip_components,
+    require          => Archive::Download["${name}.${extension}"],
+  }
+}
